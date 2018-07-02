e4d6676c17389bd5072f3acb466d5b6e93bd9775
Prakhar mathur <itprakhar@gmail.com>
2018-07-02 17:22:24 +0530

    DC Website

--- src/app/learning/learning-routing.module.ts
+++ src/app/learning/learning-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { DynamicFormsComponent } from './dynamic-forms/dynamic-forms.component';
 import { DirectiveComponent } from './directive/directive.component';
 import { ViewchildComponent } from './viewchild/viewchild.component';
+import { ParentchildComponent } from './parentchild/parentchild.component';
 
 const routes: Routes = [
   {path:"dynamic",component:DynamicFormsComponent},
   {path:"directive",component:DirectiveComponent},
-  {path:"viewchild",component:ViewchildComponent}
+  {path:"viewchild",component:ViewchildComponent},
+  {path:"parentchild",component:ParentchildComponent}
 ];
 
 @NgModule({
--- src/app/learning/learning.module.ts
+++ src/app/learning/learning.module.ts
@@ -6,12 +6,13 @@ import { DynamicFormsComponent } from './dynamic-forms/dynamic-forms.component';
 import { DirectiveComponent } from './directive/directive.component';
 import { CustomDirective } from './custom.directive';
 import { ViewchildComponent } from './viewchild/viewchild.component';
+import { ParentchildComponent } from './parentchild/parentchild.component';
 
 @NgModule({
   imports: [
     CommonModule,
     LearningRoutingModule
   ],
-  declarations: [DynamicFormsComponent, DirectiveComponent,CustomDirective, ViewchildComponent]
+  declarations: [DynamicFormsComponent, DirectiveComponent,CustomDirective, ViewchildComponent, ParentchildComponent]
 })
 export class LearningModule { }
