59ceae7140b93cedaf63049da2f20268705aea73
Prakhar mathur <itprakhar@gmail.com>
2018-08-02 03:15:16 +0530

    dynamic

--- src/app/learning/dynamic-forms/dynamic-forms.component.ts
+++ src/app/learning/dynamic-forms/dynamic-forms.component.ts
@@ -11,15 +11,24 @@ export class DynamicFormsComponent implements OnInit {
   dynamicform:FormGroup;
   constructor(private fb:FormBuilder) { 
    this.dynamicform = this.fb.group({
-    'arrayContent':new FormArray([])
+    'arrayContent':this.fb.array([this.addData()])
    });
 
   }
 
-  addControl() {
-   
-  }
+ dataItem():any{
+    this.fb.group({
+     name:new FormControl('')
+   });
+ }
+ addData() {
+  const control: FormArray = this.dynamicform.get(`arrayContent`) as FormArray;
+  control.push(this.dataItem());
+}
 
+ formCall(){
+   console.log(this.dynamicform.value)
+ }
  
 
   ngOnInit() {
